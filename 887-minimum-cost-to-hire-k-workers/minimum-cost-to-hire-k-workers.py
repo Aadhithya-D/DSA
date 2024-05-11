06b626c74f95d7b9d5760c2dd6220eec5a73ab4d
Aadhithya D <aadhithyasan024@gmail.com>
2024-05-11 10:47:49 +0530

    Time: 154 ms (64.10%) | Memory: 18.8 MB (94.07%) - LeetSync

--- 887-minimum-cost-to-hire-k-workers/minimum-cost-to-hire-k-workers.py
+++ 887-minimum-cost-to-hire-k-workers/minimum-cost-to-hire-k-workers.py
@@ -2,16 +2,15 @@ class Solution:
     def mincostToHireWorkers(self, quality: List[int], wage: List[int], k: int) -> float:
         r = float("inf")
         pairs = []
-        for i in range(len(quality)):
+        for i in range(len(wage)):
             pairs.append((wage[i]/quality[i], quality[i]))
         pairs.sort(key = lambda p:p[0])
-        
+
         maxHeap = []
         t = 0
         for rate, q in pairs:
             heapq.heappush(maxHeap, -q)
             t += q
-
             if len(maxHeap) > k:
                 t += heapq.heappop(maxHeap)
             if len(maxHeap) == k:
