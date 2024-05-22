2cf8c04dc627cd0db3d249fa3486ebadd340e7d8
Aadhithya D <aadhithyasan024@gmail.com>
2024-05-22 19:50:19 +0530

    Time: 38 ms (50.35%) | Memory: 16.6 MB (98.02%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -2,12 +2,17 @@ import copy
 
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        powerset = [[]]
-        for num in nums:
-            powersetCopy = copy.deepcopy(powerset)
-            powersetCopy.pop(-1)
-            for s in powerset:
-                s.append(num)
-            powerset.extend(powersetCopy)
-            powerset.append([])
-        return powerset
+        res = []
+        subset = []
+        def dfs(i):
+            if i >= len(nums):
+                res.append(subset.copy())
+                return
+            
+            subset.append(nums[i])
+            dfs(i+1)
+
+            subset.pop()
+            dfs(i+1)
+        dfs(0)
+        return res
